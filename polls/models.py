4ac368de6183e85a9d5cc0dc540e6c124ada1060
HolyPi <joanelly2018@gmail.com>
2020-02-05 15:49:05 -0800

    ManyToMany Fields add

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,25 @@ from django.db import models
 from django.utils import timezone
 
 
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+        
+class Group(models.Model):
+    name= models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through="Membership")
+
+    def __str__(self):
+        return self.name
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,42 @@
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
+from datetime import datetime
+from django.http import HttpResponse
+from django.views import View 
 
+from .models import Question 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+class ShowTimeView(View):
+    def get(self, request):
+        now = datetime.now()
+        html = "<html><body>It is now {}<body></html>".format(now)
+        return HttpResponse(html)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
