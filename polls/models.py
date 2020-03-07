697578915f65cd1081a8e07b69a01d70bec5787f
HolyPi <joanelly2018@gmail.com>
2020-03-06 20:48:26 -0800

    finished page 7

--- polls/models.py
+++ polls/models.py
@@ -1,44 +1,25 @@
-import datetime
 from django.db import models
 from django.utils import timezone
-
-
-
-
-class Person(models.Model):
-    name = models.CharField(max_length=128)
-
-    def __str__(self):
-        return self.name
-
-class Group(models.Model):
-    name= models.CharField(max_length=128)
-    members = models.ManyToManyField(Person, through="Membership")
-
-    def __str__(self):
-        return self.name
-
-class Membership(models.Model):
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    date_joined = models.DateField()
-    invite_reason = models.CharField(max_length=64)
+import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
     def __str__(self):
         return self.question_text
 
-
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
-
